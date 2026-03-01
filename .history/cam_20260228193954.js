1e3e83299fe43462b59a896c9bdf5718527b74ba
mmmmmmigajas <magiraldo_1283@unihumboldt.edu.co>
2026-02-28 19:59:08 -0500

    fixing

--- .history/cam_20260228193954.js
+++ .history/cam_20260228193954.js
@@ -4,7 +4,7 @@ let message =
 (velocity < 60) ? "Demasiado lento" :
     (velocity <= 80) ? "Velocidad adecuada" :
     (velocity <= 100) ? "Un poco rápido" :
-    "Multa por exceso de velocidad"
+    "Multa por exceso de velocidad";
 
 console.log(message);
 
